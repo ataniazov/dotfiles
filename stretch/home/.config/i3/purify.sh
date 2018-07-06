da1c65ea8d655c62f11cdc8b88173481a791b63b
Ata Niyazov <ata.niazov@gmail.com>
2018-07-06 20:22:33 +0300

    bug fixes

--- stretch/home/.config/i3/purify.sh
+++ stretch/home/.config/i3/purify.sh
@@ -3,8 +3,8 @@
 echo "Cleaning..."
 
 # Clean browser data
-rm -rf ~/.cache/chromium/ ~/.config/chromium/	#Chromium
-rm -rf ~/.cache/mozilla/ ~/.mozilla/		#Firefox
+rm -rf $HOME/.cache/chromium/ $HOME/.config/chromium/	#Chromium
+rm -rf $HOME/.cache/mozilla/ $HOME/.mozilla/		#Firefox
 
 # Wait 1 second
 sleep 1
@@ -12,7 +12,7 @@ sleep 1
 echo "Swaping with clean one..."
 
 # Swap with clean data
-tar -xvjf ~/Backup/.mozilla.tar.bz2 -C ~/ 
+tar -xvjf $HOME/Backup/.mozilla.tar.bz2 -C $HOME/ 
 
 # Wait 1 second
 sleep 1
--- stretch/home/.config/i3/startup.sh
+++ stretch/home/.config/i3/startup.sh
@@ -17,4 +17,4 @@ xfsettingsd &
 xfce4-power-manager &
 #redshift -l 40.7:29.9 -t 5900:5500 -g 0.8 -m randr &
 #compton -cCG --shadow-opacity=1.0 --fade-delta=3 --menu-opacity=1.0 --inactive-opacity=1.0 --inactive-dim 0.0 --dbus --detect-rounded-corners --detect-client-opacity --vsync opengl --shadow-exclude fullscreen --frame-opacity=1.0 --no-dnd-shadow --backend glx &
-#conky -c "/home/delta/.conky/SimpleClock/DArk_Simple_" &
+#conky -c "$HOME/.conky/SimpleClock/DArk_Simple_" &
