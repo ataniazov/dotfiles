f0ae6dd06f52a85d24f4b4f1aa4eca9b1817e242
Ata Niyazov <ata.niazov@gmail.com>
2019-07-14 20:59:19 +0300

    some fixes

--- buster/home/.config/i3/startup.sh
+++ buster/home/.config/i3/startup.sh
@@ -1,9 +1,10 @@
 #! /bin/sh
+
 # kill startup daemons
 #pkill redshift
-#pkill compton
 #pkill conky
 #pkill light-locker
+pkill compton
 pkill xdg-user-dirs-update
 pkill xfce4-power-manager
 pkill xfsettingsd
@@ -20,7 +21,7 @@ start-pulseaudio-x11 &
 xfsettingsd &
 xfce4-power-manager &
 xdg-user-dirs-update &
+compton -CGb --shadow-opacity=1.0 --fade-delta=3 --menu-opacity=1.0 --inactive-opacity=1.0 --inactive-dim 0.0 --dbus --detect-rounded-corners --detect-client-opacity --vsync opengl --shadow-exclude fullscreen --frame-opacity=1.0 --no-dnd-shadow --backend glx &
 #light-locker &
 #redshift -l 40.7:29.9 -t 5900:5500 -g 0.8 -m randr &
-#compton -cCG --shadow-opacity=1.0 --fade-delta=3 --menu-opacity=1.0 --inactive-opacity=1.0 --inactive-dim 0.0 --dbus --detect-rounded-corners --detect-client-opacity --vsync opengl --shadow-exclude fullscreen --frame-opacity=1.0 --no-dnd-shadow --backend glx &
 #conky -c "$HOME/.conky/SimpleClock/DArk_Simple_" &
