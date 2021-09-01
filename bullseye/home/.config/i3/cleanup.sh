fed2555a6b831658d9464434b114bdd93c6da5c2
Ata Niyazov <ataniazov>
2021-09-01 19:05:42 +0500

    some changes

--- bullseye/home/.config/i3/cleanup.sh
+++ bullseye/home/.config/i3/cleanup.sh
@@ -1,10 +1,10 @@
-#! /bin/sh
+! /bin/sh
 
 echo "Cleaning..."
 
 # Clean browser data
-rm -rf $HOME/.cache/chromium/ $HOME/.config/chromium/	#Chromium
-rm -rf $HOME/.cache/mozilla/ $HOME/.mozilla/		#Firefox
+rm -rf $HOME/.cache/chromium/ $HOME/.config/chromium/   #Chromium
+rm -rf $HOME/.cache/mozilla/ $HOME/.mozilla/            #Firefox
 
 # Wait 1 second
 sleep 1
@@ -12,7 +12,7 @@ sleep 1
 echo "Swaping with clean one..."
 
 # Swap with clean data
-tar -xvjf $HOME/Backup/.mozilla.tar.bz2 -C $HOME/ 
+tar -xvjf $HOME/Backup/.mozilla.tar.bz2 -C $HOME/
 
 # Wait 1 second
 sleep 1
